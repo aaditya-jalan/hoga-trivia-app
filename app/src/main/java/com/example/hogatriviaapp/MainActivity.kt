c9dc4c9a299e8921d7b7fadba5dea87e6f8f38ce
aadityajalan <jalanaaditya@gmail.com>
2023-05-25 19:47:39 +0530

    made some buttons

--- app/src/main/java/com/example/hogatriviaapp/MainActivity.kt
+++ app/src/main/java/com/example/hogatriviaapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hogatriviaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val startbtn=findViewById<Button>(R.id.startbutton)
+        startbtn.setOnClickListener {
+            val intent= Intent(this, category::class.java)
+            startActivity(intent)
+        }
+
     }
 }
