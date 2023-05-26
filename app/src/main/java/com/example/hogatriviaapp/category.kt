f44549f5497b8175bb99733198b3070c69ecffe4
aadityajalan <jalanaaditya@gmail.com>
2023-05-26 17:47:34 +0530

    added questions

--- app/src/main/java/com/example/hogatriviaapp/category.kt
+++ app/src/main/java/com/example/hogatriviaapp/category.kt
@@ -1,11 +1,34 @@
 package com.example.hogatriviaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class category : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+        val button1= findViewById<Button>(R.id.artsbutton)
+        val button2= findViewById<Button>(R.id.filmbutton)
+        val button3= findViewById<Button>(R.id.sportsbutton)
+        val button4= findViewById<Button>(R.id.sciencebutton)
+        button1.setOnClickListener {
+            val intent2= Intent(this, art::class.java)
+            startActivity(intent2)
+        }
+        button2.setOnClickListener {
+            val intent3= Intent(this, film::class.java)
+            startActivity(intent3)
+        }
+        button3.setOnClickListener {
+            val intent4= Intent(this,sport::class.java)
+            startActivity(intent4)
+        }
+        button4.setOnClickListener {
+            val intent5= Intent(this, question::class.java)
+            startActivity(intent5)
+        }
+
     }
 }
